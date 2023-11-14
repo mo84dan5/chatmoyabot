fc7d1bcbd4aa7dfb9b648e5c22a56fa4f889bd3a
米須諒 <r.komesu@churadata.okinawa>
2023-11-14 13:21:50 +0900

    add message to button ui

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ document.querySelector('#app').innerHTML = `
     </a>
     <h1>Hello Vite!</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      <button id="counter" type="button">push</button>
     </div>
     <p class="read-the-docs">
       Click on the Vite logo to learn more
